417285a15790454e5abd5767907f38507e2fb8fd
Nikos Rapousis <nikos.rapousis@3sidedcube.com>
2019-05-17 15:36:05 +0100

    Initial commit

---
+++ ios/Classes/PubnubPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface PubnubPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/PubnubPlugin.m
@@ -0,0 +1,20 @@
+#import "PubnubPlugin.h"
+
+@implementation PubnubPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"pubnub_plugin"
+            binaryMessenger:[registrar messenger]];
+  PubnubPlugin* instance = [[PubnubPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
